e37cfdf6d5ecb52e7472b44fb804c69c96b0f6dd
Sai Sandeep Rayanuthala <rayanuthalas@gmail.com>
2019-08-24 23:18:34 +0530

    Backend Algorith Linked

--- Backend/engagement/engagement/fileuploader/views.py
+++ Backend/engagement/engagement/fileuploader/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+import os
+import re
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from django.conf import settings
 
 # Create your views here.
 def index(request):
@@ -13,14 +17,57 @@ def uploader(request):
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'form.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
-    return render(request, 'form.html')
+        print(filename)
+        file1 = os.path.join(settings.MEDIA_ROOT, myfile.name)
+        print(file1)
+        file = open(file1,encoding="utf8")
+        c=0
+        mem = []
+        dicti={}
+        print("I reached my waypoint")
+        while True:
+            line = file.readline()
+            x = re.search(r"(\d.*?\,.*?-.*?\:)", line)
+            if x:
+                r = re.search(r"(-.*?:)",x.group()).group()[2:-1]
+                c+=1
 
+                if (r in mem): 
+                    #print ("Member Exists") 
+                    for i in dicti:
+                        if(i==r):
+                            a = dicti[i]
+                            up = {r:a+1}
+                            dicti.update(up)
+                            #print(up)
+                else:
+                    mem.append(r)
+                    up = {r:1}
+                    dicti.update(up)
+
+
+
+            if not line:
+                z=0
+                
+                break
+        for i in dicti :  
+            z+=dicti[i]
+        print(dicti)
+        print(z,c)
+        messages.info(request , dicti)
+        file.close()
+        os.remove(file1)
+    elif request.method == 'POST':
+        value=request.POST['login']
+        if value is not None:
+            redirect('login')
+    return render(request, 'form.html')
+        
 def login(request):
     if request.method == "POST":
+
+        print(settings.MEDIA_ROOT)
         phone = request.POST['number']
         password =  request.POST['password']
         print(password)
@@ -51,18 +98,18 @@ def register(request):
         if password == confirm_password:
             if User.objects.filter(email = email).exists():
                 messages.info(request , "Email Already exist")
-                return redirect('login')
+                return redirect('/login')
             elif User.objects.filter(username = phone_number).exists():
                 messages.info(request , "Phone NUmber Already Exisits")
-                return redirect('login')
+                return redirect('/login')
             else:
                 user = User.objects.create_user(first_name = first_name, last_name=last_name,username=phone_number, email=email,password=password)
                 user.save()
-                redirect('login')
+                redirect('/login')
         else:
             messages.info(request, "Password Not Matching")
             redirect('register')
-        return redirect('login/')
+        return redirect('/login')
     else:
         return render(request , 'register.html')
     
