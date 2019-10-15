a3370e5fcddde841c34f2fe307c42cd8b7c81ce4
Akshat Gupta <akshatvg23@gmail.com>
2019-10-15 14:03:17 +0530

    Final

--- fileuploader/models.py
+++ fileuploader/models.py
@@ -3,8 +3,6 @@ import uuid
 import os
 
 # Create your models here.
-
-
 def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
@@ -15,14 +13,18 @@ class MyMedia(models.Model):
     upload = models.FileField(upload_to=get_file_path)
 
 
+
+
+    
 class Post(models.Model):
     user_name = models.CharField(max_length=200, unique=False)
     slug = models.SlugField(unique=True, default=uuid.uuid1)
     file_name = models.TextField()
-    one = models.CharField(max_length=100)
-    two = models.CharField(max_length=100)
-    three = models.CharField(max_length=100)
+    one = models.CharField( max_length = 100 )
+    two = models.CharField( max_length = 100 )
+    three = models.CharField( max_length = 100 )
     created_on = models.DateTimeField(auto_now_add=True)
-
     class Meta:
         ordering = ['-created_on']
+
+    
